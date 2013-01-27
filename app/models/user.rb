4b8b1c23aba0c0eb539c068e8fc49368bf6cab33
kristenmills <kristen@kristen-mills.com>
2013-01-26 21:59:22 -0500

    fixed my has_many for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-	has_many :achievments, :solutions_submitted, :problems_solved
+	has_many :achievments
+	has_many :solutions_submitted
+	has_many :problems_solved
   attr_accessible :email, :password, :points, :salt, :username
 end
