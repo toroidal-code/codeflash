ed68fb5da58bef8bfe3d152ce9ba4fe01c19596a
Nicolas McCurdy <thenickperson@gmail.com>
2013-01-27 04:36:34 -0500

    Use intelligent navbar links that know which page should be marked active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def path_is? str
+    request.path == str
+  end
+
+  def navbar_link name, path, icon=nil
+    render partial: 'shared/navbar_link', locals: {
+      path: path,
+      name: name,
+      icon: icon
+    }
+  end
+
 end
