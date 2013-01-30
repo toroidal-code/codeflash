8ed0bf5314aff193e10c5544e1847165ca31a2bb
kristenmills <kristen@kristen-mills.com>
2013-01-29 21:47:35 -0500

    added name and about me to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 	has_many :achievments
 	has_many :solutions_submitted
 	has_many :problems_solved
-  attr_accessible :email, :password, :points, :salt, :username
+  attr_accessible :email, :password, :points, :salt, :username, :name, :about_me
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129005225) do
+ActiveRecord::Schema.define(:version => 20130130023936) do
 
   create_table "achievements", :force => true do |t|
     t.string   "name"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(:version => 20130129005225) do
     t.integer  "points"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
+    t.text     "about_me"
   end
 
 end
