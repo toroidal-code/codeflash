24e9039ee31f260717b7fa54ac3f60fcd1740c5d
kristenmills <kristen@kristen-mills.com>
2013-01-31 21:02:11 -0500

    added validation to the language

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,6 +1,7 @@
 class Achievement < ActiveRecord::Base
   belongs_to :solution
   attr_accessible :description, :name, :point_value
+  
   validates :name, :description, :point_value, :presence => true
   validates :point_value, :numericality => { :only_integer => true, :greater_than => 0 }
 end
