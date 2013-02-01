c8601c7d17bb8e9c8d86b1905f7310a412eef59a
kristenmills <kristen@kristen-mills.com>
2013-01-31 21:07:42 -0500

    added problem validation

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,4 +2,7 @@ class Problem < ActiveRecord::Base
 	has_many :solutions
 	belongs_to :user
   attr_accessible :description, :point_value, :problem_name
+
+  validates :description, :point_value, :problem_name, :presence => true
+  validates :point_value, :numericality => { :only_integer => true, :greater_than => 0 }
 end
