861d5e421ac3be7aef76df14a9a797eafd1e2ed5
Nicolas McCurdy <thenickperson@gmail.com>
2013-02-05 00:37:52 -0500

    Add sign in and sign up forms to the home page
    For now, the home page renders partials that are almost exactly the same as the
    normal sign in and sign up pages. There are also a few silly hacks here that
    will be fixed soon.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,16 @@ module ApplicationHelper
     }
   end
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
