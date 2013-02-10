3624f1820cbf54057d50e9be016b56c6524beff1
kristenmills <kristen@kristen-mills.com>
2013-02-10 17:31:37 -0500

    Omniauth should work. We'll find out when it's merged to master

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :omniauthable
   has_one :profile
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me, 
+                  :username, :login, :admin, :provider, :uid
   attr_accessor :login
   # attr_accessible :title, :body
   validates :email, :password, :presence => true
