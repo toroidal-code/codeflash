00661f4ea1758f68a8ba9e9e48ccdf5c619f579f
kristenmills <kristen@kristen-mills.com>
2013-02-10 18:18:11 -0500

    fixed the tests

--- test/functional/languages_controller_test.rb
+++ test/functional/languages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LanguagesControllerTest < ActionController::TestCase
   setup do
-    user = User.new(email: "lol@lol.lol", password: "lollollol", admin: true)
+    user = User.new(email: "lol@lol.lol", username: "LOLOLOLOLOL", password: "LOLlol101", admin: true)
     user.skip_confirmation!
     user.save
     sign_in(user)
