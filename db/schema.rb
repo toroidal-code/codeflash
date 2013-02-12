4fb495ebbf337401885a976bb87f5aa9e1020a92
kristenmills <kristen@kristen-mills.com>
2013-02-12 14:40:48 -0500

    added shortname to problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130212193649) do
+ActiveRecord::Schema.define(:version => 20130212194021) do
 
   create_table "achievements", :force => true do |t|
     t.string   "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20130212193649) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "profile_id"
+    t.string   "shortname"
   end
 
   add_index "problems", ["profile_id"], :name => "index_problems_on_profile_id"
