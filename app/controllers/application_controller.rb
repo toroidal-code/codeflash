5354c9f02e7fc4d53bbdfe92eeeda01182d7e6c6
kristenmills <kristen@kristen-mills.com>
2013-02-15 15:14:02 -0500

    made reconfirmable false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,16 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    edit_profile_path current_user.username
+    if resource_or_scope.is_a?(User)
+      edit_profile_path current_user.username
+    end
   end
 
   def after_sign_in_path_for(resource)
-   if current_user.sign_in_count == 1
+    if current_user.sign_in_count == 1 && resource_or_scope.is_a?(User)
       edit_profile_path current_user.username
+    else 
+      super
     end
   end
 end
