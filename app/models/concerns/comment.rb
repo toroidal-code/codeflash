7bfe03f8f0f1814465be6dbcc562d71291472ece
kristenmills <kristen@kristen-mills.com>
2013-02-16 01:17:31 -0500

    Added has many comments to user

--- app/models/concerns/comment.rb
+++ app/models/concerns/comment.rb
@@ -2,7 +2,7 @@ module Comment
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :user
+    belongs_to :profile
     attr_accessible :body
     validates :body, :length => {:maximum => 200}
   end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,8 @@ class Profile < ActiveRecord::Base
   has_many :solutions
   has_many :achievements, :through => :solutions
   has_many :problems
+  has_many :problem_comments
+  has_many :solution_comments
   attr_accessible :about_me, :favorite_language, :github, :name, :user_id
 
   validates :about_me, :length => {:maximum => 500}
