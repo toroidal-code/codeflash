470a6aa523aceeb26f59bca5c0f4394cf58c403f
Katherine Whitlock <toroidalcode@gmail.com>
2013-02-16 22:25:12 -0500

    Edited deploy.rb for puma tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,3 +44,33 @@ namespace :deploy do
         run "cd #{release_path}; bundle exec rake db:setup RAILS_ENV=#{rails_env}"
     end
 end
+
+#========================
+#CUSTOM
+#========================
+namespace :puma do
+  desc "Start Puma"
+    task :start, :except => { :no_release => true } do
+        run "sudo /etc/init.d/puma start #{application}"
+  end
+  after "deploy:start", "puma:start"
+
+  desc "Stop Puma"
+    task :stop, :except => { :no_release => true } do
+        run "sudo /etc/init.d/puma stop #{application}"
+  end
+  after "deploy:stop", "puma:stop"
+
+  desc "Restart Puma"
+  task :restart, roles: :app do
+        run "sudo /etc/init.d/puma restart #{application}"
+  end
+  after "deploy:restart", "puma:restart"
+
+  desc "create a shared tmp dir for puma state files"
+  task :after_symlink, roles: :app do
+        run "sudo rm -rf #{release_path}/tmp"
+        run "ln -s #{shared_path}/tmp #{release_path}/tmp"
+  end
+  after "deploy:create_symlink", "puma:after_symlink"
+end
