fa72b2a37c667fd0a5f4dd0acb8b489cf09fcfc7
Stacy Carlos <sjcarlos212@gmail.com>
2013-02-16 19:10:44 -0500

    YARD documentation for the comments controllers

--- app/controllers/problem_comments_controller.rb
+++ app/controllers/problem_comments_controller.rb
@@ -1,5 +1,10 @@
 class ProblemCommentsController < ApplicationController
   authorize_resource
+  # Creates and saves a new solution comment.
+  #
+  # POST /problem/problem_id/comment
+  #
+  # @return [String] the HTML/JSON for the problem
   def create
     @problem = Problem.find_by_shortname(params[:problem_id])
     @comment = @problem.comments.create!(params[:problem_comment])
