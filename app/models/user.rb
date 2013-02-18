e855e600fb145b71c64cfc1b99c21ccbb0581b7e
kristenmills <kristen@kristen-mills.com>
2013-02-17 19:27:14 -0500

    updated the user comments slightly

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,9 +87,9 @@ class User < ActiveRecord::Base
     end
   end
 
-  # Creates a new Profile for the current User and adds it to the database.
+  # Creates a new Profile for the new User and adds it to the database.
   #
-  # @return [Profile] the new profile, which is connected to the current User
+  # @return [Profile] the new profile, which is connected to the new User
   # and added to the database
   def create_profile
     Profile.create!(user_id: id)
