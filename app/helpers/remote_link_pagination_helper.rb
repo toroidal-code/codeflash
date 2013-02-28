2a2ebe773c8c5773fce6adfe7e5ebb2bbaf618be
Katherine Whitlock <toroidalcode@gmail.com>
2013-02-27 22:14:46 -0500

    bootstrap-pagination style!

--- app/helpers/remote_link_pagination_helper.rb
+++ app/helpers/remote_link_pagination_helper.rb
@@ -1,5 +1,5 @@
 module RemoteLinkPaginationHelper
-  class LinkRenderer < WillPaginate::ActionView::LinkRenderer
+  class LinkRenderer < WillPaginate::ActionView::BootstrapLinkRenderer
     def link(text, target, attributes = {})
       attributes['data-remote'] = true
       super
