2fb917d1ee1afb03b62b4f4afc82c2060ad763f2
kristenmills <kristen@kristen-mills.com>
2013-02-28 20:33:16 -0500

    removed the comment concern and included votabale in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
+  include Votable
   belongs_to :profile
-  belongs_to :commentable
+  belongs_to :commentable, polymorphic: true
+
+  validates :body,
+    length: { maximum: 500 },
+    presence: true
 end
