de4109d0c307514389e88a028933adec4b52df43
kristenmills <kristen@kristen-mills.com>
2013-03-28 17:23:32 -0400

    added more tests to comment

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -14,8 +14,23 @@ class CommentTest < ActiveSupport::TestCase
     comment = Comment.new({body:"x"*501})
     assert !comment.valid?
   end
+
+  test 'should not be valid upvotes not integer' do
+    comment = Comment.new({body:"x"*501, up_votes:"string"})
+    assert !comment.valid?
+  end
+
+  test 'should not be valid downvotes not integer' do
+    comment = Comment.new({body:"x"*501, down_votes:"string"})
+    assert !comment.valid?
+  end
+
   test 'should be valid' do
-    comment = Comment.new({body:@comment.body})
+    comment = Comment.new({body:@comment.body, up_votes:4, down_votes:5})
     assert comment.valid?
+  end
+
+  test 'get votes' do
+    assert_equal 90, @comment.votes
   end
 end
