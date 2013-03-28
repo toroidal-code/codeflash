42ac66c8057b0949088cf451d2388327f511d65e
kristenmills <kristen@kristen-mills.com>
2013-03-28 16:37:13 -0400

    Added test for the category Model

--- test/models/achievement_test.rb
+++ test/models/achievement_test.rb
@@ -1,36 +1,35 @@
 require 'test_helper'
-
 class AchievementTest < ActiveSupport::TestCase
   setup do
     @achievement = achievements(:one)
   end
 
-  test "should not save missing name" do
+  test "should not be valid missing name" do
     achievement = Achievement.new({ description: @achievement.description, points: @achievement.points })
     assert !achievement.valid?
   end
 
-  test "should not save missing description" do
+  test "should not be valid missing description" do
     achievement = Achievement.new({name: @achievement.name, points: @achievement.points })
     assert !achievement.valid?
   end
 
-  test "should not save missing points" do
+  test "should not be valid missing points" do
     achievement = Achievement.new({name: @achievement.name, description: @achievement.description })
     assert !achievement.valid?
   end
 
-  test "should not save points not integer" do
+  test "should not be valid points not integer" do
     achievement = Achievement.new({name: @achievement.name, description: @achievement.description, points: "string" })
     assert !achievement.valid?
   end
 
-  test "should not save points less than 0" do
+  test "should not be valid points less than 0" do
     achievement = Achievement.new({name: @achievement.name, description: @achievement.description, points: -1 })
     assert !achievement.valid?
   end
 
-  test "should not save points" do
+  test "should be valid" do
     achievement = Achievement.new({name: @achievement.name, description: @achievement.description, points: @achievement.points })
     assert achievement.valid?
   end
