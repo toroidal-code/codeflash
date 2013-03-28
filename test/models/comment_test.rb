d44ed1790764ebeb8f32b0cd938e8e3b096674b7
kristenmills <kristen@kristen-mills.com>
2013-03-28 16:51:08 -0400

    Finished comment model tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @comment = comments(:one)
+  end
+
+  test 'should not be valid no body' do
+    comment = Comment.new()
+    assert !comment.valid?
+  end
+
+  test 'should not be valid too long body' do
+    comment = Comment.new({body:"x"*501})
+    assert !comment.valid?
+  end
+  test 'should be valid' do
+    comment = Comment.new({body:@comment.body})
+    assert comment.valid?
+  end
 end
--- test/models/solution_comment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SolutionCommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
