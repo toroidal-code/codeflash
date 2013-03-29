eaa0ed61253f7d8e14cbb7e45c9a745e2b06a34e
kristenmills <kristen@kristen-mills.com>
2013-03-29 14:01:12 -0400

    Wrote tests for Problem

--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,7 +1,84 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @problem = problems(:one)
+  end
+
+  test 'should not be valid missing name' do
+    problem = Problem.new({ description: @problem.description,
+                            points: @problem.points,
+                            shortname: "myshortname9"
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should not be valid missing description' do
+    problem = Problem.new({ name: @problem.name,
+                            points: @problem.points,
+                            shortname: "myshortname9"
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should not be valid missing points' do
+    problem = Problem.new({ name: @problem.name,
+                            description: @problem.description,
+                            shortname: "myshortname9"
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should not be valid missing shortname' do
+    problem = Problem.new({ name: @problem.name,
+                            description: @problem.description,
+                            points: @problem.points
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should not be valid points not integer' do
+    problem = Problem.new({ name: @problem.name,
+                            description: @problem.description,
+                            points: "string",
+                            shortname: "myshortname9"
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should not be valid points less than zero' do
+    problem = Problem.new({ name: @problem.name,
+                            description: @problem.description,
+                            points: -1,
+                            shortname: @problem.shortname
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should not be valid shortname not unique' do
+    problem = Problem.new({ name: @problem.name,
+                            description: @problem.description,
+                            points: @problem.points,
+                            shortname: @problem.shortname
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should not be valid invalid shortname format' do
+    problem = Problem.new({ name: @problem.name,
+                            description: @problem.description,
+                            points: @problem.points,
+                            shortname: "300[[]d"
+                          })
+    assert !problem.valid?
+  end
+
+  test 'should be valid' do
+    problem = Problem.new({ name: @problem.name,
+                            description: @problem.description,
+                            points: @problem.points,
+                            shortname: "myshortname9"
+                          })
+    assert problem.valid?
+  end
 end
