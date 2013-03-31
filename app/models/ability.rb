c19953f76a3c6edb33a94b264c37821e92dfc712
kristenmills <kristen@kristen-mills.com>
2013-03-31 16:33:08 -0400

    Updated abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,7 @@ class Ability
       can :manage, :all
     else
       can :read, :all
+      cannot :read, Flag
       can :update, Profile, user_id: user.id
       can :create, [Profile, Solution, Comment ]
       can :create, Flag
