a25d887f23c640bc45c3431d3cca7879f9ef7a19
kristenmills <kristen@kristen-mills.com>
2013-03-31 16:37:01 -0400

    added model tests

--- test/models/flag_test.rb
+++ test/models/flag_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class FlagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @flag = flags(:one)
+  end
+
+  test 'should not be valid missing reason' do
+    flag = Flag.new(explanation:@flag.explanation)
+    assert !flag.valid?
+  end
+
+  test 'should not be valid missing explanation' do
+    flag = Flag.new(reason:@flag.reason)
+    assert !flag.valid?
+  end
+
+  test 'should not be valid explanation too long' do
+    flag = Flag.new(reason:@flag.reason, explanation:"f"*751)
+    assert !flag.valid?
+  end
+
+  test 'should  be valid' do
+    flag = Flag.new(reason:@flag.reason, explanation:@flag.explanation)
+    assert flag.valid?
+  end
 end
