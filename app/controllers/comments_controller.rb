df7b2ed424006e711bda6b71099751a65d3876ac
kristenmills <kristen@kristen-mills.com>
2013-03-31 22:42:18 -0400

    Added solution show page for flagging link

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,11 @@ class CommentsController < ApplicationController
     redirect_to path
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+    @solution = @comment.commentable_type == 'Solution'
+  end
+
   # Adds an upvote
   def upvote
     vote true
