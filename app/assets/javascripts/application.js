afee4d83f63ea86e8f701661754e549375d20af2
Nicolas McCurdy <thenickperson@gmail.com>
2013-04-01 17:57:59 -0400

    Fix a bug where the selector I wrote didn't catch our i tags at all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
 
 var deluxe_spinning_action = new Konami();
 deluxe_spinning_action.code = function() {
-  $('i [class^="icon-"], i [class*=" icon-"]').addClass('icon-spin');
+  $('i[class^="icon-"], i[class*=" icon-"]').addClass('icon-spin');
   $('img').addClass('spin');
 }
 deluxe_spinning_action.load();
